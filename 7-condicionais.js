17b9bccad1aa5722a03ca4f774062acabeeb6b60
birajnior <ubiratanbernardo@gmail.com>
2022-12-12 23:49:38 -0300

    add aulas

---
+++ 7-condicionais.js
@@ -0,0 +1,28 @@
+console.log('Trabalhando com condicionais');
+const listaDeDestinos = new Array(
+    'Salvador',
+    'São Paulo',
+    'Rio de Janeiro'
+)
+
+const idadeComprador = 17
+const estaAcompanhada = false
+
+console.log('Destinos possíveis: ');
+console.log(listaDeDestinos);
+
+// if (idadeComprador >= 18) {
+//     console.log('Comprador maior de idade');
+//     listaDeDestinos.splice(1, 1) // Removendo destino
+// } else if (estaAcompanhada) {
+//     //A pessoa é menor de idade
+//     console.log('Comprador está acompanhado');
+//     listaDeDestinos.splice(1, 1) // Removendo destino
+// } else {
+//     console.log('Comprador não é maior de idade, não é possivél vender.');
+// }
+
+
+
+
+console.log(listaDeDestinos);
