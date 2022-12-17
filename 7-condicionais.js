cc681177092fbadd0bc4f2e6c9551484a012ade1
birajnior <ubiratanbernardo@gmail.com>
2022-12-16 23:48:22 -0300

    Atualizando aula 7 e criando aula 8

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -5,8 +5,9 @@ const listaDeDestinos = new Array(
     'Rio de Janeiro'
 )
 
-const idadeComprador = 17
-const estaAcompanhada = false
+const idadeComprador = 14
+const estaAcompanhada = true
+const temPassagemComprada = true
 
 console.log('Destinos possíveis: ');
 console.log(listaDeDestinos);
@@ -24,5 +25,18 @@ console.log(listaDeDestinos);
 
 
 
+if (idadeComprador >= 18 || estaAcompanhada == true) {
+    console.log('Boa Viagem!!!');
+    listaDeDestinos.splice(2, 1) // Removendo destino
+} else {
+    console.log('Comprador não é maior de idade, não é possivél vender.');
+}
+
+console.log('Embarque: \n\n');
+if (idadeComprador >= 18 || estaAcompanhada == true && temPassagemComprada) {
+    console.log("Boa Viagem!");
+} else {
+    console.log('Você não pode embarcar');
+}
 
 console.log(listaDeDestinos);
