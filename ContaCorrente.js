937a10ba34e40db1981a17d33521e2c857c231f0
birajnior <ubiratanbernardo@gmail.com>
2022-12-30 01:47:47 -0300

    Adicionando package.json
    Aprendi fazer o export e import no JS

---
+++ ContaCorrente.js
@@ -0,0 +1,17 @@
+export  class ContaCorrente {
+    agencia;
+    //#saldo = 0 https://github.com/tc39/proposal-class-fields#private-fields Proprosta para metodo de privacidade.
+    _saldo = 0; // enquanto não tem nenhum metodo implementado utiliza-se o underline(_) para informar que o metodo é privado e que não deve ser mexido e/ou exibido.
+
+    sacar(valor) {
+        if (this._saldo >= valor) {
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor) {
+        if (valor <= 0) return;
+        this._saldo += valor;
+    }
+}
