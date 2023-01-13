eaadd058cc1b01c427f61cf0b9ec82e80768db26
birajnior <ubiratanbernardo@gmail.com>
2023-01-13 18:27:05 -0300

    Limpando o código
    Apendendo sobre get, set, static e constructo.

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,13 +1,35 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente {
+    static numeroDeContas = 0;
     agencia;
-    cliente;
-
-
-
-
+    _cliente;
     //#saldo = 0 https://github.com/tc39/proposal-class-fields#private-fields Proprosta para metodo de privacidade.
     _saldo = 0; // enquanto não tem nenhum metodo implementado utiliza-se o underline(_) para informar que o metodo é privado e que não deve ser mexido e/ou exibido.
 
+
+
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente() {
+        return this._cliente;
+    }
+
+    get saldo() {
+        return this._saldo;
+    }
+
+
+    constructor(cliente, agencia) {
+        this.cliente = cliente;
+        this.agencia = agencia;
+        ContaCorrente.numeroDeContas += 1;
+    }
+    
     sacar(valor) {
         if (this._saldo >= valor) {
             this._saldo -= valor;
--- index.js
+++ index.js
@@ -1,35 +1,15 @@
 import { Cliente } from "./Cliente.js"
 import { ContaCorrente } from "./ContaCorrente.js"
 
-const cliente1 = new Cliente();
-cliente1.nome = 'Ricardo';
-cliente1.cpf = 11122233309;
+const cliente1 = new Cliente('Ricardo', 11122233309);
+const cliente2 = new Cliente('Alice', 88822233309);
 
-// const cliente2 = new Cliente();
-// cliente2.nome = 'Alice';
-// cliente2.cpf = 88822233309;
+const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
+contaCorrenteRicardo.depositar(500);
 
+const conta2 = new ContaCorrente(cliente2, 102);
+conta2.depositar(1000)
+let valor = 200;
+contaCorrenteRicardo.transferir(valor, conta2);
 
-const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.agencia = 1001;
-contaCorrenteRicardo.cliente = cliente1;
-contaCorrenteRicardo.depositar(500)
-
-// contaCorrenteRicardo.depositar(-100);
-// contaCorrenteRicardo.depositar(100);
-// contaCorrenteRicardo.depositar(100);
-
-// const valorSacado = contaCorrenteRicardo.sacar(50);
-
-const conta2 = new ContaCorrente()
-conta2.cliente = new Cliente()
-conta2.cliente.nome = 'Alice'
-conta2.cliente.cpf = 88822233309
-conta2.agencia = 102
-
-let valor = 200
-contaCorrenteRicardo.transferir(valor, conta2)
-console.log(conta2);
-
-
-// console.log(contaCorrenteRicardo);
+console.log(conta2._saldo);
